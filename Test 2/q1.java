6896e544ff182a3f3ccb313b889ff6d358bfb92e
Dhrumil Bhut <85599841+dhrumilbhut@users.noreply.github.com>
2023-06-18 15:03:19 +0530

    added: Test 2

---
+++ Test 2/q1.java
@@ -0,0 +1,27 @@
+public class q1 {
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(mySqrt(x)); // Output: 2
+
+        x = 8;
+        System.out.println(mySqrt(x)); // Output: 2
+    }
+
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int first = 1, last = x;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return last;
+    }
+}
